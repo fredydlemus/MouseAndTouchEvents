40725ca3ddfacc22d13a48c409a379f0a4baace1
fredyflemus <fredyflemus@gmail.com>
2021-05-17 23:15:32 -0600

    minor fixes

--- index.js
+++ index.js
@@ -1,54 +1,63 @@
 var cuadro = document.getElementById("cuadro");
 var lienzo = cuadro.getContext("2d");
 var color = "blue";
+var yi;
+var xi;
 
 
-cuadro.addEventListener('mousedown', inicioTrazo);
-cuadro.addEventListener('touchstart', inicioTrazo);
+cuadro.addEventListener('mousedown', esMouse);
+cuadro.addEventListener('touchstart', esTouch, true);
 
 
 
+function esTouch(evento){
+    
+    evento.preventDefault();
 
-function inicioTrazo(evento){
-
-    var xi = evento.layerX;
-    var yi = evento.layerY;
-
+    xi = evento.touches[0].pageX;
+    yi = evento.touches[0].pageY;
     dibujarLinea(lienzo, color, xi, yi, xi + 2, yi+2)
-    console.log('inicio trazo');
-    cuadro.addEventListener('mousemove', enMovimiento);
+    
     cuadro.addEventListener('touchmove', enMovimientoTouch);
-    cuadro.addEventListener('mouseup', finalizoTrazo);
+    cuadro.addEventListener('touchend', () => cuadro.removeEventListener('touchmove', enMovimientoTouch));
+
+
+    function enMovimientoTouch(evento){
+        // console.log('en movimiento con touch');
+        // console.log(evento)
+        // console.log(evento.touches[0].pageX)
+
+        var xf = evento.touches[0].pageX;
+        var yf = evento.touches[0].pageY;
+        // console.log("variables:")
+        // console.log(xi + " " + yi);
+        // console.log(xf + " " + yf);
+        dibujarLinea(lienzo, color, xi, yi, xf, yf);
+        xi = xf;
+        yi = yf;
+    }
+    
+}
+
+function esMouse(evento){
+    yi = evento.layerY;
+    xi = evento.layerX;
+    dibujarLinea(lienzo, color, xi, yi, xi + 2, yi+2)
+
+    cuadro.addEventListener('mousemove', enMovimiento);
+    cuadro.addEventListener('mouseup', () => cuadro.removeEventListener('mousemove', enMovimiento));
 
     function enMovimiento(evento){
         
-        console.log('en movimiento');
-        console.log(evento)
+        // console.log('en movimiento con mouse');
+        // console.log(evento)
         var xf = evento.layerX;
         var yf = evento.layerY;
         dibujarLinea(lienzo, color, xi, yi, xf, yf);
         xi = xf;
         yi = yf;
     }
-
-    function enMovimientoTouch(evento){
-        console.log('en movimiento');
-        console.log(evento)
-        var xf = evento.touches[0].pageX;
-        var yf = evento.touches[0].pageY;
-        dibujarLinea(lienzo, color, xi, yi, xf, yf);
-        xi = xf;
-        yi = yf;
-    }
-
-    function finalizoTrazo(){
-    
-        console.log("finalizo linea");
-        cuadro.removeEventListener('mousemove', enMovimiento);
-    }
 }
-
-
 
 function dibujarLinea(lienzo, color, xinicial, yinical, xfinal, yfinal){
     lienzo.beginPath(); 
